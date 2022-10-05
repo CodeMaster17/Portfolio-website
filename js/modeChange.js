5280b4aea8ff5e2d75741388ee356f9bedf120b1
Harshit <2105891@kiit.ac.in>
2022-10-06 02:16:57 +0530

    started with new UI

--- js/modeChange.js
+++ js/modeChange.js
@@ -1,23 +1,28 @@
 // for changing light and dark mode
-let darkModeBtn = document.getElementById("darkModeBtn");
-let lightModeBtn = document.getElementById("lightModeBtn");
 
-let heroArea = document.getElementById("home");
+let lightMode = document.getElementById("lightMode");
+let darkMode = document.getElementById("darkMode");
 
+let outerContainer = document.getElementById("outerContainer");
 
+let myNameHeading = document.getElementById("myNameHeading");
 
-darkModeBtn.onclick=()=>{
-    console.log("button clicked");
-    darkModeBtn.style.display = 'none';
-    lightModeBtn.style.display = 'block';
-    
-    
-    
-}
-lightModeBtn.onclick=()=>{
-    console.log("button clicked");
-    darkModeBtn.style.display = 'block';
-    lightModeBtn.style.display = 'none';
+let staticText = document.getElementById("staticText");
 
-    heroArea.style.background = "white";
-}
+let resumeBtn = document.getElementById("resumeBtn");
+let resumeBtnTxt = document.getElementById("resumeBtnTxt");
+
+lightMode.addEventListener("click", () => {
+
+    lightMode.style.display = "none"
+    darkMode.style.display = "block"
+    darkMode.style.backgroundColor = "black"
+    darkMode.style.color = "white"
+
+    outerContainer.style.backgroundColor = "white"; //background color of whole container
+    myNameHeading.style.color = "black"; //HARSHIT YADAV text
+    staticText.style.color = "#B3AA99"; //I'm text
+    resumeBtn.style.backgroundColor = "#FACF0F"; //resume button
+    resumeBtnTxt.style.color = "black"; //text inside resume button
+    resumeBtnTxt.style.fontWeight = "1000"; //text inside resume button
+})
---
+++ js/projects.js
@@ -0,0 +1,7 @@
+let personalProjectBtn = document.getElementById("personalProjectBtn");
+
+PersonalProjects = document.getElementById("PersonalProjects")
+
+personalProjectBtn.addEventListener("click", () => {
+    PersonalProjects.style.display = "flex"
+})
