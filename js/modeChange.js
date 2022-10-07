dbc57017352d3d43b33fad18a0c8684ea6b27a22
Harshit <2105891@kiit.ac.in>
2022-10-07 22:28:34 +0530

    changes

--- js/modeChange.js
+++ js/modeChange.js
@@ -1,7 +1,9 @@
 // for changing light and dark mode
 
 let lightMode = document.getElementById("lightMode");
+
 let darkMode = document.getElementById("darkMode");
+let darkModeBIcon = document.getElementById("darkModeBIcon");
 
 let outerContainer = document.getElementById("outerContainer");
 
@@ -12,12 +14,17 @@ let staticText = document.getElementById("staticText");
 let resumeBtn = document.getElementById("resumeBtn");
 let resumeBtnTxt = document.getElementById("resumeBtnTxt");
 
+let fontMain = document.getElementsByClassName("fontMain");
+let AbhisekName = document.getElementById("AbhisekName");
+
 lightMode.addEventListener("click", () => {
 
     lightMode.style.display = "none"
     darkMode.style.display = "block"
     darkMode.style.backgroundColor = "black"
-    darkMode.style.color = "white"
+    // darkMode.children.style.backgroundColor = "black"
+    // darkMode.style.color = "white"
+    // darkModeBIcon.style.color = "black";
 
     outerContainer.style.backgroundColor = "white"; //background color of whole container
     myNameHeading.style.color = "black"; //HARSHIT YADAV text
@@ -25,4 +32,17 @@ lightMode.addEventListener("click", () => {
     resumeBtn.style.backgroundColor = "#FACF0F"; //resume button
     resumeBtnTxt.style.color = "black"; //text inside resume button
     resumeBtnTxt.style.fontWeight = "1000"; //text inside resume button
+
+
+    fontMain[0].style.color = "black";
+    AbhisekName.style.color = "#FACF0F"
+
+
+
+
+
+})
+
+darkMode.addEventListener('click', () => {
+
 })
